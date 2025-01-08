39da7f749dc07651e6b28365350865c758bf3e13
mateusprodev <mateusferreiraprodev@gmail.com>
2025-01-08 13:36:17 -0300

    add css do rodape

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,7 @@ const Footer = () => {
 
   return (
     <footer className="footer">
+        <hr className="rainbow-hr"/>
       {/* Seção Principal */}
       <div className="footer-container">
         {/* Menu recolhível */}
@@ -25,31 +26,18 @@ const Footer = () => {
             <li><a href="/faq">FAQ</a></li>
           </ul>
         </div>
-
-        {/* Redes sociais */}
-        <div className="footer-socials">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src="./img/face.png" alt="Facebook" />
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src="./img/insta.png" alt="Instagram" />
-          </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src="./img/what.png" alt="Whatssap" />
-          </a>
-        </div>
+        <div className="security">
+            <img src="./img/protect.png" alt="Site Seguro" />
+            <span>Site 100% Seguro</span>
+          </div>
 
         {/* Segurança e bandeiras de pagamento */}
         <div className="footer-payments-security">
-          <div className="security">
-            <img src="./img/security-icon.png" alt="Site Seguro" />
-            <span>Site 100% Seguro</span>
-          </div>
           <div className="payment-flags">
             <img src="./img/visa.png" alt="Visa" />
             <img src="./img/mastercard.png" alt="MasterCard" />
             <img src="./img/pix.png" alt="Pix" />
-            <img src="./img/elo.png" alt="Elo" />
+            <img className="elocard"src="./img/elo.png" alt="Elo" />
           </div>
         </div>
       </div>
